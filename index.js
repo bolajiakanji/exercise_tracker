42c4c2582155ecbdff851721ea429ef89dfad248
lukman bolaji <bolajiakanji21@gmail.com>
2024-07-08 00:22:17 -0700

    add public and views folder

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const port = process.env.NODE_env || 3000;
 
 mongoose
-    .connect("mongodb://localhost/metadata")
-    .then(() => console.log("connected to mangodb"))
-    .then(err => console.error("failed to connect to mangodb", err));
+  .connect("mongodb://localhost/metadata")
+  .then(() => console.log("connected to mangodb"))
+  .then((err) => console.error("failed to connect to mangodb", err));
 
-    app.use(cors())
+app.use(cors());
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ encoded: false }))
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'views/index.html')
+})
 
- 
+app.get()
 
-
-
-const listen = app.listen(port, () => console.log('listening on port' + listen.address().port))
+const listen = app.listen(port, () =>
+  console.log("listening on port" + listen.address().port)
+);
